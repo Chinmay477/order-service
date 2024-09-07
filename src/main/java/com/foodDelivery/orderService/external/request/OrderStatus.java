f12e0a67d73b8a73399b697b7963c18e605c7a7d
Chinmay477 <chinmaychatterji477@gmail.com>
2024-09-07 18:54:02 +0530

    Adding Order Status

--- src/main/java/com/foodDelivery/orderService/external/request/OrderStatus.java
+++ src/main/java/com/foodDelivery/orderService/external/request/OrderStatus.java
@@ -2,14 +2,17 @@ package com.foodDelivery.orderService.external.request;
 
 public enum OrderStatus {
 
+    NEW_ORDER,
+    ORDER_PROCESSING,
     PAYMENT_PENDING,
     PAYMENT_PROCESSING,
-    PAYMENT_COMPLETE,
-    PLACED,
-    IN_PREPARATION,
-    IN_TRANSIT,
-    DELIVERED,
-    CANCELLED;
+    PAYMENT_SUCCESS,
+    PAYMENT_FAILURE,
+    ORDER_CONFIRMED,
+    ORDER_PREPARING,
+    ORDER_IN_TRANSIT,
+    ORDER_DELIVERED,
+    ORDER_CANCELLED;
 
     public static boolean isValid(String value) {
         for (OrderStatus status : values()) {
